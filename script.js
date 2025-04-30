8299af5a1f834c87d4ac70ea7440e9754296b8ac
EEE95 <144793581+EEE95@users.noreply.github.com>
2025-04-30 16:39:16 +0200

    Header og footer stylet, funktion tilføjet

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* ---- Navigation ---- */
+
 const menuToggle = document.getElementById('menu-toggle');
 const navbar = document.getElementById('navbar');
 const dropdowns = document.querySelectorAll('.dropdown');
@@ -6,12 +8,28 @@ menuToggle.addEventListener('click', () => {
   navbar.classList.toggle('active');
 });
 
-// Dropdowns (mobil)
 dropdowns.forEach(dropdown => {
   dropdown.addEventListener('click', function (e) {
     if (window.innerWidth <= 768) {
-      e.preventDefault();
+      e.preventDefault(); 
       this.classList.toggle('open');
     }
   });
 });
+
+/* Aktiv side */
+const currentPage = window.location.pathname.split("/").pop();
+
+const navLinks = document.querySelectorAll('.navbar a');
+
+navLinks.forEach(link => {
+  const linkPage = link.getAttribute('href');
+
+  if (linkPage === currentPage || (currentPage === '' && linkPage === 'index.html')) {
+    link.classList.add('active');
+  } else {
+    link.classList.remove('active');
+  }
+});
+
+
