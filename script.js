cca5d5f1c0c8fbc87483263942c9eac3dea3f7a9
EEE95 <144793581+EEE95@users.noreply.github.com>
2025-05-07 21:36:34 +0200

    to the top btn tilføjet

--- script.js
+++ script.js
@@ -66,3 +66,20 @@ document.getElementById('contactForm').addEventListener('submit', (e) => {
 document.querySelector('.close').addEventListener('click', () => {
   document.getElementById("myModal").style.display = "none";
 });
+
+/* ---- TO THE TOP BTN ---- */
+const mybutton = document.getElementById("to-top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
